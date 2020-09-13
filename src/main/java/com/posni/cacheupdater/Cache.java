68b4d9a64b9d5286edf57022427fa0bc722fe07a
Jagadeesh Posni <Posni_Jagadeesh@gap.com>
2020-09-13 23:50:21 +0530

    Minor refactorings

--- src/main/java/com/posni/cacheupdater/Cache.java
+++ src/main/java/com/posni/cacheupdater/Cache.java
@@ -2,38 +2,45 @@ package com.posni.cacheupdater;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Optional;
-import java.util.function.Supplier;
 
+/**
+ * The actual cache class with a TTL.
+ * @param <T>
+ */
 public class Cache<T> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private T healthCheckResult;
-	private long cacheSavedTime;
-	private final long ttl;
+	private T value;//The value stored in cache
+	private long cacheSavedTime;//Epoch time as and when the cache is saved in value
+	private final long ttl;//the maxismum amount of time in ms for which the cache has to be stored
 
 	public Cache(final long ttl) {
 		this.ttl = ttl;
 	}
 
 	public synchronized T get() {
-		if (this.healthCheckResult == null)
+		if (this.value == null)
 			return null;
 
+		//Before returning the value, if value's age is less than TTL, then return null
 		final long now = Instant.now().toEpochMilli();
-		if (now - this.cacheSavedTime > this.ttl) {
-			this.healthCheckResult = null;
+		if (getCacheAge(now) > this.ttl) {
+			this.value = null;
 			this.cacheSavedTime = 0;
 			return null;
 		}
 
-		return this.healthCheckResult;
+		return this.value;
+	}
+
+	private long getCacheAge(long now) {
+		return now-this.cacheSavedTime;
 	}
 
 	public synchronized void set(final T obj) {
 		System.out.println("Inside Set: Setting the cache...");
-		this.healthCheckResult = obj;
+		this.value = obj;
 		this.cacheSavedTime = Instant.now().toEpochMilli();
 	}
 }
